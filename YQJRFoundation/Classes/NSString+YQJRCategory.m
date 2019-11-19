bc356dd7080150f5adea0cb8e69575eac6fe4ef4
wangzhen <wangzhen@qianyujiashu.cn>
2019-11-19 17:28:51 +0800

    添加散列函数并测试是否存在内存泄漏问题

--- YQJRFoundation/Classes/NSString+YQJRCategory.m
+++ YQJRFoundation/Classes/NSString+YQJRCategory.m
@@ -6,7 +6,7 @@
 //
 
 #import "NSString+YQJRCategory.h"
-#import <CommonCrypto/CommonDigest.h>
+#import <CommonCrypto/CommonCrypto.h>
 
 @implementation NSString (YQJRCategory)
 
@@ -44,17 +44,217 @@
     return result;
 }
 
-- (NSString *)yqjr_md5 {
-    const char *cStr = [self UTF8String];
-    unsigned char result[16];
-    CC_MD5(cStr,(CC_LONG)strlen(cStr), result);
-    return [NSString stringWithFormat:
-            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-            result[0], result[1], result[2], result[3],
-            result[4], result[5], result[6], result[7],
-            result[8], result[9], result[10], result[11],
-            result[12], result[13], result[14], result[15]
-            ];
+#pragma mark - 散列函数
+- (NSString *)yqjr_md5String {
+    const char *str = self.UTF8String;
+    unsigned char buffer[CC_MD5_DIGEST_LENGTH];
+    
+    CC_MD5(str, (CC_LONG)strlen(str), buffer);
+    
+    return [self stringFromBytes:buffer length:CC_MD5_DIGEST_LENGTH];
+}
+
+- (NSString *)yqjr_sha1String {
+    const char *str = self.UTF8String;
+    unsigned char buffer[CC_SHA1_DIGEST_LENGTH];
+    
+    CC_SHA1(str, (CC_LONG)strlen(str), buffer);
+    
+    return [self stringFromBytes:buffer length:CC_SHA1_DIGEST_LENGTH];
+}
+
+- (NSString *)yqjr_sha256String {
+    const char *str = self.UTF8String;
+    unsigned char buffer[CC_SHA256_DIGEST_LENGTH];
+    
+    CC_SHA256(str, (CC_LONG)strlen(str), buffer);
+    
+    return [self stringFromBytes:buffer length:CC_SHA256_DIGEST_LENGTH];
+}
+
+- (NSString *)yqjr_sha512String {
+    const char *str = self.UTF8String;
+    unsigned char buffer[CC_SHA512_DIGEST_LENGTH];
+    
+    CC_SHA512(str, (CC_LONG)strlen(str), buffer);
+    
+    return [self stringFromBytes:buffer length:CC_SHA512_DIGEST_LENGTH];
+}
+
+#pragma mark - HMAC 散列函数
+- (NSString *)yqjr_hmacMD5StringWithKey:(NSString *)key {
+    const char *keyData = key.UTF8String;
+    const char *strData = self.UTF8String;
+    unsigned char buffer[CC_MD5_DIGEST_LENGTH];
+    
+    CCHmac(kCCHmacAlgMD5, keyData, strlen(keyData), strData, strlen(strData), buffer);
+    
+    return [self stringFromBytes:buffer length:CC_MD5_DIGEST_LENGTH];
+}
+
+- (NSString *)yqjr_hmacSHA1StringWithKey:(NSString *)key {
+    const char *keyData = key.UTF8String;
+    const char *strData = self.UTF8String;
+    unsigned char buffer[CC_SHA1_DIGEST_LENGTH];
+    
+    CCHmac(kCCHmacAlgSHA1, keyData, strlen(keyData), strData, strlen(strData), buffer);
+    
+    return [self stringFromBytes:buffer length:CC_SHA1_DIGEST_LENGTH];
+}
+
+- (NSString *)yqjr_hmacSHA256StringWithKey:(NSString *)key {
+    const char *keyData = key.UTF8String;
+    const char *strData = self.UTF8String;
+    unsigned char buffer[CC_SHA256_DIGEST_LENGTH];
+    
+    CCHmac(kCCHmacAlgSHA256, keyData, strlen(keyData), strData, strlen(strData), buffer);
+    
+    return [self stringFromBytes:buffer length:CC_SHA256_DIGEST_LENGTH];
+}
+
+- (NSString *)yqjr_hmacSHA512StringWithKey:(NSString *)key {
+    const char *keyData = key.UTF8String;
+    const char *strData = self.UTF8String;
+    unsigned char buffer[CC_SHA512_DIGEST_LENGTH];
+    
+    CCHmac(kCCHmacAlgSHA512, keyData, strlen(keyData), strData, strlen(strData), buffer);
+    
+    return [self stringFromBytes:buffer length:CC_SHA512_DIGEST_LENGTH];
+}
+
+#pragma mark - 文件散列函数
+
+#define FileHashDefaultChunkSizeForReadingData 4096
+
+- (NSString *)yqjr_fileMD5Hash {
+    NSFileHandle *fp = [NSFileHandle fileHandleForReadingAtPath:self];
+    if (fp == nil) {
+        return nil;
+    }
+    
+    CC_MD5_CTX hashCtx;
+    CC_MD5_Init(&hashCtx);
+    
+    while (YES) {
+        @autoreleasepool {
+            NSData *data = [fp readDataOfLength:FileHashDefaultChunkSizeForReadingData];
+            
+            CC_MD5_Update(&hashCtx, data.bytes, (CC_LONG)data.length);
+            
+            if (data.length == 0) {
+                break;
+            }
+        }
+    }
+    [fp closeFile];
+    
+    unsigned char buffer[CC_MD5_DIGEST_LENGTH];
+    CC_MD5_Final(buffer, &hashCtx);
+    
+    return [self stringFromBytes:buffer length:CC_MD5_DIGEST_LENGTH];
+}
+
+- (NSString *)yqjr_fileSHA1Hash {
+    NSFileHandle *fp = [NSFileHandle fileHandleForReadingAtPath:self];
+    if (fp == nil) {
+        return nil;
+    }
+    
+    CC_SHA1_CTX hashCtx;
+    CC_SHA1_Init(&hashCtx);
+    
+    while (YES) {
+        @autoreleasepool {
+            NSData *data = [fp readDataOfLength:FileHashDefaultChunkSizeForReadingData];
+            
+            CC_SHA1_Update(&hashCtx, data.bytes, (CC_LONG)data.length);
+            
+            if (data.length == 0) {
+                break;
+            }
+        }
+    }
+    [fp closeFile];
+    
+    unsigned char buffer[CC_SHA1_DIGEST_LENGTH];
+    CC_SHA1_Final(buffer, &hashCtx);
+    
+    return [self stringFromBytes:buffer length:CC_SHA1_DIGEST_LENGTH];
+}
+
+- (NSString *)yqjr_fileSHA256Hash {
+    NSFileHandle *fp = [NSFileHandle fileHandleForReadingAtPath:self];
+    if (fp == nil) {
+        return nil;
+    }
+    
+    CC_SHA256_CTX hashCtx;
+    CC_SHA256_Init(&hashCtx);
+    
+    while (YES) {
+        @autoreleasepool {
+            NSData *data = [fp readDataOfLength:FileHashDefaultChunkSizeForReadingData];
+            
+            CC_SHA256_Update(&hashCtx, data.bytes, (CC_LONG)data.length);
+            
+            if (data.length == 0) {
+                break;
+            }
+        }
+    }
+    [fp closeFile];
+    
+    unsigned char buffer[CC_SHA256_DIGEST_LENGTH];
+    CC_SHA256_Final(buffer, &hashCtx);
+    
+    return [self stringFromBytes:buffer length:CC_SHA256_DIGEST_LENGTH];
+}
+
+- (NSString *)yqjr_fileSHA512Hash {
+    NSFileHandle *fp = [NSFileHandle fileHandleForReadingAtPath:self];
+    if (fp == nil) {
+        return nil;
+    }
+    
+    CC_SHA512_CTX hashCtx;
+    CC_SHA512_Init(&hashCtx);
+    
+    while (YES) {
+        @autoreleasepool {
+            NSData *data = [fp readDataOfLength:FileHashDefaultChunkSizeForReadingData];
+            
+            CC_SHA512_Update(&hashCtx, data.bytes, (CC_LONG)data.length);
+            
+            if (data.length == 0) {
+                break;
+            }
+        }
+    }
+    [fp closeFile];
+    
+    unsigned char buffer[CC_SHA512_DIGEST_LENGTH];
+    CC_SHA512_Final(buffer, &hashCtx);
+    
+    return [self stringFromBytes:buffer length:CC_SHA512_DIGEST_LENGTH];
+}
+
+#pragma mark - 助手方法
+/**
+ *  返回二进制 Bytes 流的字符串表示形式
+ *
+ *  @param bytes  二进制 Bytes 数组
+ *  @param length 数组长度
+ *
+ *  @return 字符串表示形式
+ */
+- (NSString *)stringFromBytes:(unsigned char *)bytes length:(int)length {
+    NSMutableString *strM = [NSMutableString string];
+    
+    for (int i = 0; i < length; i++) {
+        [strM appendFormat:@"%02x", bytes[i]];
+    }
+    
+    return [strM copy];
 }
 
 @end
